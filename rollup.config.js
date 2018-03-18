93fda95c79e0d54966eb10893a0898304d7ca12b
Jeremy Holcomb <jholcomb@nextgen.com>
2018-03-18 13:24:51 -0700

    update rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,50 +1,62 @@
+import { buildExternalHelpers, transform } from '@babel/core';
 import aliasModuleSpecifiers from 'rollup-plugin-alias-module-specifiers';
-import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
+import { minify } from 'uglify-js';
+import { promisify } from 'util';
 
-export default [
-    {
-        input: 'src/main.js',
-        output: {
-            file: 'dist/bundle.js',
-            format: 'iife'
-        },
-        plugins: [
-            aliasModuleSpecifiers({
-                'react': './node_modules/react/umd/react.production.min.js',
-                'react-dom': './node_modules/react-dom/umd/react-dom.production.min.js'
-            }),
-            babel({
-                babelrc: false,
-                exclude: 'node_modules/**',
-                plugins: [
-                    'external-helpers',
-                    'transform-react-jsx'
-                ],
-                presets: [
-                    'stage-3',
-                    ['env', {
-                        targets: {
-                            browsers: ["last 2 versions", "ie >= 11"]
-                        },
-                        modules: false
-                    }],
-                    'minify'
-                ]
-            }),
-            commonjs()
-        ]
+const transformAsync = promisify(transform);
+
+export default {
+    input: 'src/main.js',
+    output: {
+        file: 'dist/main.min.js',
+        format: 'iife'
     },
-    {
-        input: 'src/polyfills.js',
-        output: {
-            file: 'dist/polyfills.js',
-            format: 'es'
+    plugins: [
+        {
+            name: 'babel-transform',
+            transform(source) {
+                return transformAsync(source, {
+                    ast: false,
+                    babelrc: false,
+                    compact: true,
+                    plugins: [
+                        '@babel/plugin-external-helpers',
+                        '@babel/plugin-transform-react-jsx'
+                    ],
+                    presets: [
+                        '@babel/preset-stage-3',
+                        ['@babel/preset-env', {
+                            targets: {
+                                browsers: [
+                                    'last 2 versions',
+                                    'ie >= 11'
+                                ]
+                            },
+                            modules: false
+                        }]
+                    ]
+                });
+            }
         },
-        plugins: [
-            aliasModuleSpecifiers({
-                'babel-polyfill': './node_modules/babel-polyfill/dist/polyfill.min.js'
-            })
-        ]
-    }
-];
+        aliasModuleSpecifiers({
+            'react': './node_modules/react/umd/react.production.min.js',
+            'react-dom': './node_modules/react-dom/umd/react-dom.production.min.js'
+        }),
+        commonjs({
+            sourceMap: false
+        }),
+        {
+            name: 'external-babelHelpers',
+            transformBundle(bundledSource) {
+                return buildExternalHelpers() + bundledSource;
+            }
+        },
+        {
+            name: 'uglify',
+            transformBundle(bundledSource) {
+                return minify(bundledSource).code;
+            }
+        }
+    ]
+};
